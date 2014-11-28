ddcead94d0d120913490a62ea7efc48884dd820c
didstriker <didstriker@gmail.com>
2014-11-28 16:05:15 +0100

    Inicial

---
+++ Clases/Modelo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public static class Map
+    {
+        string[,] map = new string[15, 15];
+
+        public string[,] Map { get { return map; } private set { map = value; } }
+    }
+
+    public class Player
+    {
+
+    }
+
+
+}
